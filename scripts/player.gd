32fe25d4a5ad1bb1c0cac74d61b8930179ee0f5a
unknown <kevjrodgers@gmail.com>
2025-09-29 01:18:04 -0700

    Added player jumping sound

--- scripts/player.gd
+++ scripts/player.gd
@@ -3,7 +3,8 @@ extends CharacterBody2D
 var isDead: bool = false
 const SPEED = 130.0
 const JUMP_VELOCITY = -300.0
-@onready var animated_sprite: AnimatedSprite2D = $AnimatedSprite2D
+@onready var animated_sprite: AnimatedSprite2D = $AnimatedSprite2D 
+@onready var jump_sound: AudioStreamPlayer2D = $JumpSound
 
 func die():
 	isDead = true
@@ -21,6 +22,7 @@ func _physics_process(delta: float) -> void:
 		# Handle jump.
 		if Input.is_action_just_pressed("jump") and is_on_floor():
 			velocity.y = JUMP_VELOCITY
+			jump_sound.play()
 
 		# Get the input direction (-1, 0, or 1) and handle the movement/deceleration.
 		# As good practice, you should replace UI actions with custom gameplay actions.
@@ -43,7 +45,7 @@ func _physics_process(delta: float) -> void:
 				animated_sprite.play("run")
 		else:
 			animated_sprite.play("jump")
-
+			
 		# Applies movement
 		if direction:
 			velocity.x = direction * SPEED
