7104fef3179e79a79d09baf557f67c2af644bc58
unknown <kevjrodgers@gmail.com>
2025-09-29 00:44:57 -0700

    Added a player death animation

--- scripts/killzone.gd
+++ scripts/killzone.gd
@@ -2,13 +2,11 @@ extends Area2D
 
 @onready var timer: Timer = $Timer
 
-
 func _on_body_entered(body: Node2D) -> void:
 	# When the player enters the Killzone's body, a timer starts
 	# Once the timer is completed, timeout() will run, which will reload/restart the scene
 	print("You have died...")
-	if body.has_method("die"):
-		body.die()
+	body.die()
 	Engine.time_scale = 0.5 # Sets to half speed
 	body.get_node("CollisionShape2D").queue_free()
 	timer.start()
